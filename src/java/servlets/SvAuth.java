42755c1255bdcd2b76ec489b8033148a761c0785
Gabriel Shimabuku <gaby.shima12@gmail.com>
2021-12-17 05:51:04 -0300

    Set admin admin as username and password

--- src/java/servlets/SvAuth.java
+++ src/java/servlets/SvAuth.java
@@ -10,17 +10,16 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import logic.Controller;
 
-
 @WebServlet(name = "SvAuth", urlPatterns = {"/SvAuth"})
 public class SvAuth extends HttpServlet {
+
     //Instancio la clase Controller de la lógica
     Controller control = new Controller();
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-   
-    }
 
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -28,27 +27,27 @@ public class SvAuth extends HttpServlet {
         processRequest(request, response);
     }
 
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        
+
         boolean isAuthorized = control.auth(username, password);
-        
-        if(isAuthorized == true){
+        if (username.equals("admin") && password.equals("admin")) {
+            isAuthorized = true;
+        }
+        if (isAuthorized == true) {
             HttpSession mySession = request.getSession(true);
             mySession.setAttribute("username", username);
             mySession.setAttribute("password", password);
-            
+
             response.sendRedirect("pages/homePage/home.jsp");
         } else {
             response.sendRedirect("index.jsp");
         }
     }
-
 
     @Override
     public String getServletInfo() {
